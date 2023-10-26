5d17bdbdc95676cbdc4c3cde560fbef4df4155b2
Martinesko <87262511+Martinesko@users.noreply.github.com>
2023-10-27 01:35:36 +0300

    Create db context truck table and repository

---
+++ Server/Server.Data/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Data.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        ValueTask<TEntity> GetByIdAsync(string id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ Server/Server.Data/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Data.Interfaces;
+using Server.Data.Interfaces.Repositories;
+using Server.Data.Repositories;
+
+namespace Server.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AlgonaDbContext context;
+
+        private readonly ITruckRepository truckRepository;
+
+        public UnitOfWork(AlgonaDbContext context, ITruckRepository truckRepository)
+        {
+            this.context = context;
+            this.truckRepository = truckRepository;
+        }
+
+        public ITruckRepository Trucks => truckRepository ?? new TruckRepository(context);
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CommitAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
