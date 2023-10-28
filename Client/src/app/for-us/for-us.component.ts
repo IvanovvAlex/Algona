e04ad1ad48c61bf59c422489f7ac77a23b865cb9
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-10-28 23:23:35 +0300

    For Us  component
    Create For Us component

---
+++ Client/src/app/for-us/for-us.component.ts
@@ -0,0 +1,36 @@
+import { Component} from '@angular/core';
+import { ApiService } from '../api.service';
+
+
+
+@Component({
+  selector: 'app-for-us',
+  templateUrl: './for-us.component.html',
+  styleUrls: ['./for-us.component.css']
+})
+export class ForUsComponent {
+
+  data:any;
+  badRequest: boolean = false;
+
+    constructor(private httpService: ApiService) {}
+
+    ngOnInit() {
+      
+      this.httpService.getData().subscribe(
+        response => { 
+          if(response.status !== 200) {
+          this.badRequest = true;
+        } else {this.data = response.body,
+          console.log(this.data);
+        }},
+        error => {
+          if(error.status !== 200) {
+            this.badRequest = true;
+          }
+        }
+        
+        
+      ) 
+    }
+}
