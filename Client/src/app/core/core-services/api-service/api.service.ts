8002e0e14cafce0f8b49dd2d925abc0221349fdc
Denitsa Slavkova <deni9309@gmail.com>
2023-11-09 20:05:21 +0200

    US113: Create Garage component

--- Client/src/app/core/core-services/api-service/api.service.ts
+++ Client/src/app/core/core-services/api-service/api.service.ts
@@ -3,20 +3,23 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { ContactFormData } from 'src/app/shared/typization/interfaces/ContactFormData';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  private url = 'api/forus/data'
+    private url = 'api/forus/data'
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  getData() {
-    return this.http.get(this.url, { observe: 'response' });
-  }
+    getData() {
+        return this.http.get(this.url, { observe: 'response' });
+    }
 
-  sendData(contactFormData: ContactFormData) {
-    return this.http.post('api/contact/', contactFormData, { observe: 'response' })
-  }
+    sendData(contactFormData: ContactFormData) {
+        return this.http.post('api/contact/', contactFormData, { observe: 'response' })
+    }
 
+    getGaragePhotos() {
+        return this.http.get('api/garage/photos');
+    }
 }
