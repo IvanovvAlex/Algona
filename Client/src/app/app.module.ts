3facd4912af247d9655e9bd3a980d4e195f35cdd
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-11-15 17:12:20 +0200

    Transport component and Spedition component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { ReactiveFormsModule } from "@angular/forms";
 
 // Component imports
 import { AppComponent } from './app.component';
@@ -15,6 +16,10 @@ import { HeroComponent } from './components-WIP-folder/hero/hero.component';
 import { ContactFormComponent } from './components-WIP-folder/contact-form/contact-form.component';
 import { NotFoundComponent } from './components-WIP-folder/not-found/not-found.component';
 import { GarageComponent } from './components-WIP-folder/garage/garage.component';
+import { SpeditionRequestComponent } from './components-WIP-folder/spedition-request/spedition-request.component';
+import { TransportRequestComponent } from './components-WIP-folder/transport-request/transport-request.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     declarations: [
@@ -24,6 +29,7 @@ import { GarageComponent } from './components-WIP-folder/garage/garage.component
         ContactFormComponent,
         NotFoundComponent,
         GarageComponent
+        
     ],
     imports: [
         AuthModule,
@@ -32,7 +38,12 @@ import { GarageComponent } from './components-WIP-folder/garage/garage.component
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        HttpClientModule
+        HttpClientModule,
+        TransportRequestComponent,
+        SpeditionRequestComponent,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule
     ],
     providers: [],
     bootstrap: [ AppComponent ]
