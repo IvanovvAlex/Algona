3ad2a15c27165905804ede39b1825592d02b0225
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2023-12-14 22:37:09 +0200

    Revert "Revert "app user inherite identity user and all entity add isdelete prop""
    This reverts commit 4d5a7ee909a7ef65528970ad2a378effa087a0c3.

---
+++ Server/Server.API/Controllers/AuthController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Server.Common.Requests.AuthRequests;
+using Server.Data.Entites;
+
+namespace Server.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AuthController(
+          UserManager<User> _userManager,
+          SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpPost]
+        public async Task Register(CreateRegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new NotImplementedException();
+            }
+
+            var user = new User()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            };
+
+            var result = await userManager.CreateAsync(user, request.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.PasswordSignInAsync(user, request.Password, false, false);
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+        }
+
+        [HttpPost]
+        public async Task Login(CreateLoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new NotImplementedException();
+            }
+
+            var user = await userManager.FindByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Wrong user!");
+                throw new NotImplementedException();
+            }
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, request.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    throw new NotImplementedException();
+
+                  //TODO
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login!");
+        }
+    }
+}
