c1e619e8d63b661833e4582bc8d473b801805012
Denitsa Slavkova <deni9309@gmail.com>
2023-12-18 00:40:22 +0200

    Update About Us layout

--- Client/src/app/components-WIP-folder/for-us/for-us.component.ts
+++ Client/src/app/components-WIP-folder/for-us/for-us.component.ts
@@ -1,37 +1,10 @@
-import { Component} from '@angular/core';
-import { ApiService } from '../../core/core-services/api-service/api.service';
-
-
-// Moved Api service inside core-service folder
-
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-for-us',
-  templateUrl: './for-us.component.html',
-  styleUrls: ['./for-us.component.css']
+    selector: 'app-for-us',
+    templateUrl: './for-us.component.html',
+    styleUrls: [ './for-us.component.css' ]
 })
 export class ForUsComponent {
-
-  data:any;
-  badRequest: boolean = false;
-
-    constructor(private httpService: ApiService) {}
-
-    ngOnInit() {
-      
-      this.httpService.getData().subscribe(
-        response => { 
-          if(response.status !== 200) {
-          this.badRequest = true;
-        } else {this.data = response.body,
-          console.log(this.data);
-        }},
-        error => {
-            this.badRequest = true;
-        }
-        
-        
-      ) 
-    }
+    constructor() { }
 }
