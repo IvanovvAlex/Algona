f5f50baf0947d15aa16787f20136ea5dab85c24d
eLiminatedd <dimitur.blagoev.public@gmail.com>
2023-12-23 16:21:48 +0200

    Fixing Contact Form
    Adding more Validation
    Adding real static info
    Adding more style fixes

--- Client/src/app/features/contact/contact-form/contact-form.component.ts
+++ Client/src/app/features/contact/contact-form/contact-form.component.ts
@@ -2,72 +2,79 @@ import { Component } from '@angular/core';
 import { ApiService } from 'src/app/core/core-services/api-service/api.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { emailVdtr } from 'src/app/auth/validators/emailValidator';
+
+  @Component({
+    selector: 'app-contact-form',
+    templateUrl: './contact-form.component.html',
+    styleUrls: ['./contact-form.component.css']
+  })
+  export class ContactFormComponent {
+    response: Object | undefined;
+    error: boolean | undefined;
 
 
-@Component({
-  selector: 'app-contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
-})
-export class ContactFormComponent {
-  response: Object | undefined;
-  error: boolean | undefined;
-
-
-  contactForm = this.fb.group({
-    name: ['', [Validators.required]],
-    companyName: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    phone: ['', [Validators.required]],
-    data: ['', [Validators.required]]
-  }
-  )
-
-  sendData() {
-    if (this.contactForm.invalid) {
-      return
+    contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      companyName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, emailVdtr()]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      data: ['', [Validators.required, Validators.minLength(10)]]
     }
-
-    const { name, companyName, email, phone, data } = this.contactForm.value;
-
-    const contactFormData = {
-      name: name as string,
-      companyName: companyName as string,
-      email: email as string,
-      phone: phone as string,
-      data: data as string
-    }
-
-    this.apiService.sendData(contactFormData).subscribe(
-      {
-        next: (response) => {
-          if (response.status !== 200) {
-            this.popUp(`Error ${response.status}: ${response.statusText}`)
-          } else {
-            this.popUp('Succes - Your message has been sent to us!')
-          }
-        },
-        error: (error) => {
-          this.popUp(`Error ${error.status}: ${error.statusText}`)
-          // this.popUp('Succes - Your message has been sent to us!')
-          // ^^ to test visuals until a working API response is present
-        }
-      }
-
     )
 
+    sendData() {
+      if (this.contactForm.invalid) {
+        return
+      }
+
+      const { name, companyName, email, phone, data } = this.contactForm.value;
+
+      const contactFormData = {
+        name: name as string,
+        companyName: companyName as string,
+        email: email as string,
+        phone: phone as string,
+        data: data as string
+      }
+
+      this.apiService.sendData(contactFormData).subscribe(
+        {
+          next: (response) => {
+            if (response.status !== 200) {
+              this.popUp(`Error ${response.status}: ${response.statusText}`)
+            } else {
+              this.popUp('Succes - Your message has been sent to us!')
+            }
+          },
+          error: (error) => {
+            this.popUp(`Error ${error.status}: ${error.statusText}`)
+            // this.popUp('Succes - Your message has been sent to us!')
+            // ^^ to test visuals until a working API response is present
+          }
+        }
+
+      )
+
+    }
+
+    popUp(message: string): void {
+      this.snackBar.open(message, 'Close', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+      })
+    }
+
+    constructor(private fb: FormBuilder, private apiService: ApiService, private snackBar: MatSnackBar) {
+
+    }
+
   }
 
-  popUp(message: string): void {
-    this.snackBar.open(message, 'Close', {
-      duration: 3000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-    })
-  }
+/* TODO:
+Fix screen scroll at around 800px width
+add validation 
+Shrink map abit with a new flex container
 
-  constructor(private fb: FormBuilder, private apiService: ApiService, private snackBar: MatSnackBar) {
-
-  }
-
-}
+*/
