ca7b5fd8d93f32407d6d6b42d46ab432ed7d915a
iliyazz <100124829+iliyazz@users.noreply.github.com>
2024-01-03 22:41:08 +0200

    Add comments to RequestTransportController

--- Server/Server.API/Controllers/RequestTransportController.cs
+++ Server/Server.API/Controllers/RequestTransportController.cs
@@ -6,22 +6,47 @@
     using Server.Data.Entities;
     using Server.Domain.Interfaces;
 
+    /// <summary>
+    /// Request transport controller
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class RequestTransportController : ControllerBase
     {
         private readonly IRequestTransportService requestTransportService;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="requestTransportService"></param>
         public RequestTransportController(IRequestTransportService requestTransportService)
         {
             this.requestTransportService = requestTransportService;
         }
 
+        /// <summary>
+        /// Creates a new request for transport
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPost("Create")]
         public async Task<TransportRequest> Create(TransportRequest request)
         {
             RequestTransport requestTransport = await requestTransportService.Create(request);
 
             return request;
+        }
+
+        /// <summary>
+        /// Gets all requests for transport
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("All")]
+        public async Task<IEnumerable<RequestTransport>> All()
+        {
+            IEnumerable<RequestTransport> requests = await requestTransportService.GetAll();
+
+            return requests;
         }
     }
 }
