1b97f88de477b79058644cd23b4a120006a37f3f
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-04 19:34:58 +0200

    Add comments to Request Spedition entity

--- Server/Server.Data/Entities/RequestSpedition.cs
+++ Server/Server.Data/Entities/RequestSpedition.cs
@@ -43,29 +43,53 @@ namespace Server.Data.Entities
         [Comment("From date")]
         public DateTime FromDate { get; set; }
 
+        /// <summary>
+        /// To date
+        /// </summary>
         [Required]
         [DataType(DataType.Date)]
+        [Comment("To date")]
         public DateTime ToDate { get; set; }
 
+        /// <summary>
+        /// Number of pallets
+        /// </summary>
         [Required]
         [Range(EntityValidationConstants.Spedition.NumberOfPalletsMinValue, EntityValidationConstants.Spedition.NumberOfPalletsMaxValue)]
+        [Comment("Number of pallets")]
         public int NumberOfPallets { get; set; }
 
+        /// <summary>
+        /// Total weight
+        /// </summary>
         [Required]
         [Range(EntityValidationConstants.Spedition.TotalWeightMinValue, EntityValidationConstants.Spedition.TotalWeightMaxValue)]
+        [Comment("Total weight")]
         public int TotalWeight { get; set; }
 
+        /// <summary>
+        /// Name
+        /// </summary>
         [Required]
         [MinLength(EntityValidationConstants.Spedition.NameMinLength)]
         [MaxLength(EntityValidationConstants.Spedition.NameMaxLength)]
+        [Comment("Name")]
         public string Name { get; set; } = null!;
 
+        /// <summary>
+        /// Phone number
+        /// </summary>
         [Required]
         [Phone]
+        [Comment("Phone number")]
         public string PhoneNumber { get; set; } = null!;
 
+        /// <summary>
+        /// Email address
+        /// </summary>
         [Required]
         [EmailAddress]
+        [Comment("Email address")]
         public string Email { get; set; } = null!;
 
         public bool IsDeleted { get; set; } = false;
