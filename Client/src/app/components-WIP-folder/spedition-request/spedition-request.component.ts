3e537f4aeafdc32fb55c4e240655b3547ee6121e
Denitsa Slavkova <deni9309@gmail.com>
2024-01-04 17:59:18 +0200

    Add footer component

--- Client/src/app/components-WIP-folder/spedition-request/spedition-request.component.ts
+++ Client/src/app/components-WIP-folder/spedition-request/spedition-request.component.ts
@@ -1,10 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+
 import {
   FormGroup,
   FormBuilder,
@@ -17,8 +12,8 @@ import {
   selector: 'app-spedition-request',
   templateUrl: './spedition-request.component.html',
   styleUrls: ['./spedition-request.component.css'],
-  standalone: true,
-  imports: [CommonModule,MatFormFieldModule, MatInputModule, MatSelectModule,MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
+ // standalone: true,
+ // imports: [CommonModule,MatFormFieldModule, MatInputModule, MatSelectModule,MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
 })
 export class SpeditionRequestComponent {
 
--- Client/src/app/components-WIP-folder/transport-request/transport-request.component.ts
+++ Client/src/app/components-WIP-folder/transport-request/transport-request.component.ts
@@ -1,10 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+
 import {
   FormGroup,
   FormBuilder,
@@ -19,8 +14,8 @@ import {
   selector: 'app-transport-request',
   templateUrl: './transport-request.component.html',
   styleUrls: ['./transport-request.component.css'],
-  standalone: true,
-  imports: [CommonModule,MatFormFieldModule, MatInputModule, MatSelectModule,MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
+  //standalone: true,
+  //imports: [CommonModule,MatFormFieldModule, MatInputModule, MatSelectModule,MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
 })
 export class TransportRequestComponent implements OnInit  {
    
