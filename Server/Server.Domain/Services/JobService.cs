7f1282cbfa4de6340ac2592a2980ee8f6fb56f40
iliyazz <100124829+iliyazz@users.noreply.github.com>
2024-01-04 19:24:08 +0200

    Add comments to JobService

--- Server/Server.Domain/Services/JobService.cs
+++ Server/Server.Domain/Services/JobService.cs
@@ -4,6 +4,9 @@ using Server.Domain.Interfaces;
 
 namespace Server.Domain.Services
 {
+    /// <summary>
+    /// Job service
+    /// </summary>
     public class JobService : IJobService
     {
         private readonly IUnitOfWork unitOfWork;
@@ -11,11 +14,20 @@ namespace Server.Domain.Services
         {
             this.unitOfWork = unitOfWork;
         }
+        /// <summary>
+        /// Returns all jobs
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<Job>> GetAll()
         {
             return await unitOfWork.Jobs.GetAllAsync();
         }
 
+        /// <summary>
+        /// Returns a job by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<Job?> GetById(string id)
         {
             var job = await unitOfWork.Jobs.GetByIdAsync(id);
