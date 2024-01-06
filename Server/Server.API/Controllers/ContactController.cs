5826fb55911b4dbc4d949b1e9992a6f3f556e550
Spasko <patsokatsarski1@gmail.com>
2024-01-06 14:04:26 +0200

    Adding comments to ContactController

--- Server/Server.API/Controllers/ContactController.cs
+++ Server/Server.API/Controllers/ContactController.cs
@@ -5,6 +5,9 @@ using Server.Domain.Interfaces;
 
 namespace Server.API.Controllers
 {
+    /// <summary>
+    /// Contact controller
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ContactController : ControllerBase
@@ -16,6 +19,11 @@ namespace Server.API.Controllers
             this.sharedService = sharedService;
         }
 
+        /// <summary>
+        /// Sends request
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task Send(CreateContactRequest request)
         {
