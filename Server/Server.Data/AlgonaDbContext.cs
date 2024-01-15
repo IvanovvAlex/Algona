bddecd026592fc9fbfb16bc79cc9ad96e7257b8f
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2024-01-16 00:13:25 +0200

    feed roles and admin to db

--- Server/Server.Data/AlgonaDbContext.cs
+++ Server/Server.Data/AlgonaDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Server.Data.Configurations;
 using Server.Data.Entites;
 using Server.Data.Entities;
+
+using static Server.Common.Constants.GlobalConstants;
 
 namespace Server.Data
 {
@@ -15,16 +18,24 @@ namespace Server.Data
         public DbSet<Cargo> Cargoes { get; set; }
 
         public DbSet<Job> Jobs { get; set; }
-        
+
         public DbSet<Transport> Transports { get; set; }
-        
+
         public DbSet<Spedition> Speditions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.ApplyConfigurationsFromAssembly(typeof(TruckEntityTypeConfiguration).Assembly);
-
             base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new TruckEntityTypeConfiguration());
+
+            builder.ApplyConfiguration(new RoleConfiguration());
+
+            builder.ApplyConfiguration(new UserConfuguration());
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+               new IdentityUserRole<string>() { RoleId = AdminRoleId, UserId = AdminId }
+               );
         }
     }
 }
---
+++ Server/Server.Data/Configurations/UserConfuguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Server.Data.Entites;
+
+using static Server.Common.Constants.GlobalConstants;
+
+namespace Server.Data.Configurations
+{
+    internal class UserConfuguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(CreateUser());
+        }
+
+        private User CreateUser()
+        {
+            var hasher = new PasswordHasher<User>();
+
+            var user = new User()
+            {
+                Id = AdminId,
+                FirstName = AdminFirstName,
+                LastName = AdminLastName,
+                Email = MainEmail,
+                NormalizedEmail = MainEmail.ToUpper(),
+                UserName = MainEmail,
+                NormalizedUserName = MainEmail.ToUpper(),
+                ConcurrencyStamp = AdminConcurrencyStamp
+            };
+            user.PasswordHash = hasher.HashPassword(user, AdminPassword);
+
+            return user;
+        }
+    }
+}
