9b9f2501119cae44304cee9f5f3189711ac7eb6b
eLiminatedd <126146757+eLiminatedd@users.noreply.github.com>
2024-01-16 15:17:22 +0200

    fixing footer
    switching typography to a monospace type for phone numbers paragraphs
    making footer stick to bottom of page

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare let AOS: any;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: [ './app.component.css' ]
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
     title = 'Client';
+
+    constructor(private router: Router) {
+
+    }
+
+    hasRoute() {
+
+        if (this.router.url.match('^[\/{1}]$')) {
+            return true
+        } else if (this.router.url.includes('/home')) {
+            return true
+        }
+        //  else if (this.router.url.includes('/404')) {
+        //     return true
+        // } 
+        else {
+            return false
+        }
+
+
+    }
 
     ngOnInit(): void {
         AOS.init();
