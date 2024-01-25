d3ee750e822a2507e00743178c0e8688c23f886d
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-25 18:38:47 +0200

    Added reset password functionality

---
+++ Server/Server.Common/Requests/AuthRequests/ResetPasswordRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Server.Common.Constants.EntityValidationConstants;
+
+namespace Server.Common.Requests.AuthRequests
+{
+    public class ResetPasswordRequest
+    {
+        /// <summary>
+        /// User's email address
+        /// </summary>
+        [Required, EmailAddress]
+        [MinLength(Register.EmailMinLength)]
+        [MaxLength(Register.EmailMaxLength)]
+        public string Token { get; set; } = null!;
+
+        /// <summary>
+        /// User's password
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(Register.PasswordMinLength)]
+        [MaxLength(Register.PasswordMaxLength)]
+        public string Password { get; set; } = null!;
+
+        /// <summary>
+        /// User's confirm password
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [MinLength(Register.ConfirmPasswordMinLength)]
+        [MaxLength(Register.ConfirmPasswordMaxLength)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
