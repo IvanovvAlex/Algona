096651f7e41c44319509909dae114a21ff18b399
eLiminatedd <126146757+eLiminatedd@users.noreply.github.com>
2024-02-03 03:07:26 +0200

    contact form bug fix (#129)
    * Bugfixing Admin Tables 2
    
    * bug fixing 1
    
    * bug fixez 2
    
    * removing contact form error text
    
    * bug fixing 3
    
    * bug fixes 4
    
    * bug fixes 5

--- Client/src/app/features/contact/contact-form/contact-form.component.ts
+++ Client/src/app/features/contact/contact-form/contact-form.component.ts
@@ -1,78 +1,81 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/core/core-services/api-service/api.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroupDirective, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { emailVdtr } from 'src/app/auth/validators/emailValidator';
 
-  @Component({
-    selector: 'app-contact-form',
-    templateUrl: './contact-form.component.html',
-    styleUrls: ['./contact-form.component.css']
-  })
-  export class ContactFormComponent {
-    response: Object | undefined;
-    error: boolean | undefined;
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  response: Object | undefined;
+  error: boolean | undefined;
 
 
-    contactForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      companyName: ['', [Validators.required, Validators.minLength(2)]],
-      email: ['', [Validators.required, emailVdtr()]],
-      phone: ['', [Validators.required, Validators.minLength(10)]],
-      data: ['', [Validators.required, Validators.minLength(10)]]
+  contactForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    companyName: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, emailVdtr()]],
+    phone: ['', [Validators.required, Validators.minLength(10)]],
+    data: ['', [Validators.required, Validators.minLength(10)]]
+  }
+  )
+
+  sendData(formDirective: FormGroupDirective) {
+    if (this.contactForm.invalid) {
+      return
     }
+
+    const { name, companyName, email, phone, data } = this.contactForm.value;
+
+    const contactFormData = {
+      name: name as string,
+      companyName: companyName as string,
+      email: email as string,
+      phone: phone as string,
+      description: data as string
+    }
+
+    this.apiService.sendContactData(contactFormData).subscribe(
+      {
+        next: (response) => {
+          if (response.status !== 201) {
+            this.popUp(`Error ${response.status}: ${response.type}`)
+          } else {
+            this.popUp('Succes - Your message has been sent to us!')
+          }
+          this.contactForm.reset();
+          formDirective.resetForm();
+        },
+        error: (error) => {
+          this.popUp(`Error ${error.status}: ${error.statusText}`)
+
+          this.contactForm.reset();
+          formDirective.resetForm();
+          // this.contactForm.markAllAsTouched();
+
+          // this.popUp('Succes - Your message has been sent to us!')
+          // ^^ to test visuals until a working API response is present
+        }
+      }
+
     )
 
-    sendData() {
-      if (this.contactForm.invalid) {
-        return
-      }
+  }
 
-      const { name, companyName, email, phone, data } = this.contactForm.value;
+  popUp(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    })
+  }
 
-      const contactFormData = {
-        name: name as string,
-        companyName: companyName as string,
-        email: email as string,
-        phone: phone as string,
-        description: data as string
-      }
-
-      this.apiService.sendContactData(contactFormData).subscribe(
-        {
-          next: (response) => {
-            if (response.status !== 201) {
-              this.popUp(`Error ${response.status}: ${response.type}`)
-            } else {
-              this.popUp('Succes - Your message has been sent to us!')
-            }
-          },
-          error: (error) => {
-            this.popUp(`Error ${error.status}: ${error.statusText}`)
-            
-            this.contactForm.reset();
-            // this.contactForm.markAllAsTouched();
-            
-            // this.popUp('Succes - Your message has been sent to us!')
-            // ^^ to test visuals until a working API response is present
-          }
-        }
-
-      )
-
-    }
-
-    popUp(message: string): void {
-      this.snackBar.open(message, 'Close', {
-        duration: 3000,
-        horizontalPosition: 'center',
-        verticalPosition: 'top',
-      })
-    }
-
-    constructor(private fb: FormBuilder, private apiService: ApiService, private snackBar: MatSnackBar) {
-
-    }
+  constructor(private fb: FormBuilder, private apiService: ApiService, private snackBar: MatSnackBar) {
 
   }
+
+}
 
